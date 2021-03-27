2c60c6ca521b7c5c23f2575967c8b27818122f7a
kr1994 <karailiev1994@gmail.com>
2021-03-27 11:55:24 +0200

    Created Colours.java

---
+++ src/main/java/com/java/smart_garage/models/Colours.java
@@ -0,0 +1,41 @@
+package com.java.smart_garage.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "colours")
+public class Colours {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "colour_id")
+    private int colourId;
+
+    @Column(name = "colour")
+    private String colour;
+
+    public Colours() {
+    }
+
+    public Colours(int colourId, String colour) {
+        this.colourId = colourId;
+        this.colour = colour;
+    }
+
+    public void setColourId(int colourId) {
+        this.colourId = colourId;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getColourId() {
+        return colourId;
+    }
+}
+
