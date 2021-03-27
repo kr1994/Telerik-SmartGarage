c1a97f72a6c0b7b8087ec124b379a58e1236dfcf
kr1994 <karailiev1994@gmail.com>
2021-03-27 11:29:51 +0200

    Create RegistrationPlates.java

---
+++ src/main/java/com/java/smart_garage/models/RegistrationPlates.java
@@ -0,0 +1,42 @@
+package com.java.smart_garage.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "registation_plates")
+public class RegistrationPlates {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "plate_id")
+    private int plateId;
+
+    @Column(name = "plate_number")
+    private String plateNumber;
+
+    public RegistrationPlates() {
+    }
+
+    public RegistrationPlates(int plateId, String plateNumber) {
+        this.plateId = plateId;
+        this.plateNumber = plateNumber;
+    }
+
+
+    public void setPlateId(int plateId) {
+        this.plateId = plateId;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public int getPlateId() {
+        return plateId;
+    }
+}
