1f9b1c146631f96136503f3f70f78e55f6c37b00
P.Chipev <plamenchipev16@gmail.com>
2021-03-27 11:08:14 +0200

    Create HibernateConfiguration.java class

---
+++ src/main/java/com/java/smart_garage/configuration/HibernateConfiguration.java
@@ -0,0 +1,55 @@
+package com.java.smart_garage.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateConfiguration {
+
+    private final String dbUrl, dbUsername, dbPassword;
+
+    @Autowired
+    public HibernateConfiguration(Environment env) {
+        dbUrl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dbPassword = env.getProperty("database.password");
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.java.smart_garage.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+        return hibernateProperties;
+    }
+}
+
+
