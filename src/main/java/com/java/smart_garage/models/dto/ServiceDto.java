02cf4ebcb5c41a368bad5d4f71a65cad8e6608b9
P.Chipev <plamenchipev16@gmail.com>
2021-03-28 02:06:07 +0200

    Fix ServiceDto

--- src/main/java/com/java/smart_garage/models/dto/ServiceDto.java
+++ src/main/java/com/java/smart_garage/models/dto/ServiceDto.java
@@ -2,14 +2,18 @@ package com.java.smart_garage.models.dto;
 
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 public class ServiceDto {
 
-    @NotBlank
+    @NotNull
+    @Size(min=5,message = "Service name must be longer than 5 characters.")
     private String serviceName;
 
-    @Positive
+    @NotNull
+    @Positive(message = "Service price must be positive.")
     private double servicePrice;
 
     public ServiceDto() {
