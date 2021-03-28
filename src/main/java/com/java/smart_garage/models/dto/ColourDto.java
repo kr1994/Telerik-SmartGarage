06ecff1cc9b74be63a38f5417266f9ba02687ccd
P.Chipev <plamenchipev16@gmail.com>
2021-03-28 02:02:59 +0200

    Create ColourDto

---
+++ src/main/java/com/java/smart_garage/models/dto/ColourDto.java
@@ -0,0 +1,27 @@
+package com.java.smart_garage.models.dto;
+
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class ColourDto {
+
+    @NotNull
+    @Size(min = 2 , max = 20 , message = "Colour length must be between 2 and 20 characters.")
+    @Pattern(regexp = "^\0[0-9]" )
+    private String name;
+
+    public ColourDto() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
