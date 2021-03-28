8ee5cfbca4a1376e6fbb73cfccdd78784534bc1c
P.Chipev <plamenchipev16@gmail.com>
2021-03-28 16:15:27 +0300

    Modify DuplicateEntityException

--- src/main/java/com/java/smart_garage/exceptions/DuplicateEntityException.java
+++ src/main/java/com/java/smart_garage/exceptions/DuplicateEntityException.java
@@ -2,16 +2,15 @@ package com.java.smart_garage.exceptions;
 
 public class DuplicateEntityException extends RuntimeException {
 
-    public DuplicateEntityException(String type, int id) {
-        this(type, "id", String.valueOf(id));
+    public DuplicateEntityException(String type, String attribute, String value) {
+        super(String.format("%s with %s %s already exists.", type, attribute, value));
     }
 
-    public DuplicateEntityException(String entity, String attribute, String value) {
-
-        super(String.format("%s with %s %s already exists", entity, attribute, value));
+    public DuplicateEntityException(String type, String userName, String email, String valueUn, String valueEmail) {
+        super(String.format("%s with %s/%s %s/%s already exists.", type, userName, email, valueUn, valueEmail));
     }
 
-    public DuplicateEntityException(String message) {
-        super(message);
+    public DuplicateEntityException(String type, String attribute, int value) {
+        super(String.format("%s with %s %s already exists.", type, attribute, value));
     }
 }
