b41e95198eb0138db95a5dd52738df3e540d57a0
P.Chipev <plamenchipev16@gmail.com>
2021-03-28 02:01:27 +0200

    Create ManufacturerDto

---
+++ src/main/java/com/java/smart_garage/models/dto/ManufacturerDto.java
@@ -0,0 +1,22 @@
+package com.java.smart_garage.models.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class ManufacturerDto {
+
+    @NotNull
+    @Size(min = 2 , max = 20, message = "Manufacturer name must be between 2 and 20 characters.")
+    private String name;
+
+    public ManufacturerDto() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
