d82ea9db93905947259c9b44b8b146ceafaab315
P.Chipev <plamenchipev16@gmail.com>
2021-03-28 02:02:47 +0200

    Create CustomerDto

---
+++ src/main/java/com/java/smart_garage/models/dto/CustomerDto.java
@@ -0,0 +1,35 @@
+package com.java.smart_garage.models.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+public class CustomerDto {
+
+    @NotNull
+    @Positive(message = "User Id must be positive.")
+    private int userId;
+
+    @NotNull
+    @Size(min = 10,max = 10, message = "Phone number must be 10 characters long.")
+    private int phoneNumber;
+
+    public CustomerDto() {
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+}
