0bc846b61c6889fb6476891514a873c7940e2b7e
kr1994 <karailiev1994@gmail.com>
2021-03-31 19:51:20 +0300

    Added update method in CustomerServiceImpl

--- src/main/java/com/java/smart_garage/controllers/rest/CustomerController.java
+++ src/main/java/com/java/smart_garage/controllers/rest/CustomerController.java
@@ -60,18 +60,19 @@ public class CustomerController {
         }
     }
 
-    @PostMapping
-    public Customer update(@RequestHeader HttpHeaders headers, @Valid @RequestBody CustomerDto customerDto) {
+    @PutMapping("/{id}")
+    public Customer update(@RequestHeader HttpHeaders headers, @PathVariable int id, @Valid @RequestBody CustomerDto customerDto) {
         try {
             User user = authenticationHelper.tryGetUser(headers);
             Customer customer = modelConversionHelper.customerFromDto(customerDto);
-            //service.update(customer, user);
+            service.update(customer, user);
             return customer;
         } catch (DuplicateEntityException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
     }
 
+    @DeleteMapping("/{id}")
     public void delete(@RequestHeader HttpHeaders headers, @PathVariable int id) {
         try {
             User user = authenticationHelper.tryGetUser(headers);
