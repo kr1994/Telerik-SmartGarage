b8e7df69c9bca51bc716f0fa359f321bc361ed7b
P.Chipev <plamenchipev16@gmail.com>
2021-04-02 10:47:22 +0300

    Create EngineController

---
+++ src/main/java/com/java/smart_garage/controllers/rest/EngineController.java
@@ -0,0 +1,73 @@
+package com.java.smart_garage.controllers.rest;
+
+
+import com.java.smart_garage.ModelMaper.ModelConversionHelper;
+import com.java.smart_garage.configuration.AuthenticationHelper;
+import com.java.smart_garage.contracts.serviceContracts.EngineService;
+import com.java.smart_garage.exceptions.DuplicateEntityException;
+import com.java.smart_garage.exceptions.EntityNotFoundException;
+import com.java.smart_garage.models.Engine;
+import com.java.smart_garage.models.User;
+import com.java.smart_garage.models.dto.EngineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("smartgarage/engines")
+public class EngineController {
+
+    private final EngineService service;
+    private final ModelConversionHelper modelConversionHelper;
+    private final AuthenticationHelper authenticationHelper;
+
+
+    @Autowired
+    public EngineController(EngineService service,
+                       ModelConversionHelper modelConversionHelper,
+                       AuthenticationHelper authenticationHelper) {
+        this.service = service;
+        this.modelConversionHelper = modelConversionHelper;
+        this.authenticationHelper = authenticationHelper;
+    }
+
+    @GetMapping
+    public List<Engine> getAllEngines(){
+        return service.getAllEngines();
+    }
+
+    @GetMapping("/{id}")
+    public Engine getById(@PathVariable int id) {
+        try {
+            return service.getById(id);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    @PostMapping
+    public Engine create(@RequestHeader HttpHeaders headers, @Valid @RequestBody EngineDto engineDto) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            Engine engine = modelConversionHelper.engineFromDto(engineDto);
+            service.create(engine, user);
+            return engine;
+        } catch (DuplicateEntityException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@RequestHeader HttpHeaders headers, @PathVariable int id) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            service.delete(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
