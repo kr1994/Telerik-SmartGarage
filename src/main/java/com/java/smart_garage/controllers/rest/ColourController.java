01971c8725a606b1c263e756fad31a8f2c15c0af
P.Chipev <plamenchipev16@gmail.com>
2021-04-02 10:46:57 +0300

    Create ColourController

---
+++ src/main/java/com/java/smart_garage/controllers/rest/ColourController.java
@@ -0,0 +1,80 @@
+package com.java.smart_garage.controllers.rest;
+
+
+import com.java.smart_garage.ModelMaper.ModelConversionHelper;
+import com.java.smart_garage.configuration.AuthenticationHelper;
+import com.java.smart_garage.contracts.serviceContracts.ColourService;
+import com.java.smart_garage.exceptions.DuplicateEntityException;
+import com.java.smart_garage.exceptions.EntityNotFoundException;
+import com.java.smart_garage.models.Colour;
+import com.java.smart_garage.models.User;
+import com.java.smart_garage.models.dto.ColourDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("smartgarage/model")
+public class ColourController {
+
+    private final ColourService service;
+    private final ModelConversionHelper modelConversionHelper;
+    private final AuthenticationHelper authenticationHelper;
+
+    @Autowired
+    public ColourController(ColourService service,
+                            ModelConversionHelper modelConversionHelper,
+                            AuthenticationHelper authenticationHelper) {
+        this.service = service;
+        this.modelConversionHelper = modelConversionHelper;
+        this.authenticationHelper = authenticationHelper;
+    }
+    @GetMapping
+    public List<Colour> getAllModels(){
+        return  service.getAllColours();
+    }
+
+    @GetMapping("/{id}")
+    public Colour getById(@PathVariable int id) {
+        try {
+            return service.getById(id);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @GetMapping("/search")
+    public Colour getByName(String name) {
+        try {
+            return service.getByName(name);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    @PostMapping
+    public Colour create(@RequestHeader HttpHeaders headers, @Valid @RequestBody ColourDto colourDto) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            Colour colour = modelConversionHelper.colourFromDto(colourDto);
+            service.create(colour, user);
+            return colour;
+        } catch (DuplicateEntityException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@RequestHeader HttpHeaders headers, @PathVariable int id) {
+        try {
+            User user = authenticationHelper.tryGetUser(headers);
+            service.delete(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
