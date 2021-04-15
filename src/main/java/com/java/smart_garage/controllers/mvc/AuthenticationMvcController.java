b950cd2b4c85a7c25c014b095ba2d8cb7a6e749e
kr1994 <karailiev1994@gmail.com>
2021-04-15 16:32:32 +0300

    AuthenticationMvcController updated

--- src/main/java/com/java/smart_garage/controllers/mvc/AuthenticationMvcController.java
+++ src/main/java/com/java/smart_garage/controllers/mvc/AuthenticationMvcController.java
@@ -1,16 +1,54 @@
 package com.java.smart_garage.controllers.mvc;
 
+import com.java.smart_garage.configuration.AuthenticationHelper;
+import com.java.smart_garage.exceptions.AuthenticationHelperException;
+import com.java.smart_garage.models.dto.LoginDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping
 public class AuthenticationMvcController {
 
+    private final AuthenticationHelper authenticationHelper;
+
+    @Autowired
+    public AuthenticationMvcController(AuthenticationHelper authenticationHelper) {
+        this.authenticationHelper = authenticationHelper;
+    }
+
     @GetMapping("/login")
-    public String showLoginPage() {
+    public String showLoginPage(Model model) {
+        model.addAttribute("loginDto", new LoginDto());
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(@Valid @ModelAttribute("loginDto") LoginDto dto,
+                              BindingResult bindingResult,
+                              HttpSession session) {
+
+        if(bindingResult.hasErrors()) {
+            return "login";
+        }
+
+        try {
+            authenticationHelper.verifyAuthentication(dto.getUsername(), dto.getPassword());
+            session.setAttribute("currentUserUsername", dto.getUsername());
+        } catch (AuthenticationHelperException e) {
+            bindingResult.rejectValue("username", "auth_error", e.getMessage());
+            return "login";
+        }
+        return "redirect:/";
     }
 
     @GetMapping("/register")
