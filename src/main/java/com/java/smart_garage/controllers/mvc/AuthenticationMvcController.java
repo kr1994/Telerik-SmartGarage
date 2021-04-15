4722469b9ce662d95a1b8578052365d0a78824fe
kr1994 <karailiev1994@gmail.com>
2021-04-15 13:06:41 +0300

    Basic AuthenticationMvcController created

---
+++ src/main/java/com/java/smart_garage/controllers/mvc/AuthenticationMvcController.java
@@ -0,0 +1,21 @@
+package com.java.smart_garage.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class AuthenticationMvcController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+}
