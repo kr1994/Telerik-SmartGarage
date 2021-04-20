b1d2c7f13edebc321a8b01087a3beab68e2e5375
kr1994 <karailiev1994@gmail.com>
2021-04-21 02:26:29 +0300

    Email is sending right credentials

--- src/main/java/com/java/smart_garage/services/EmailServiceImpl.java
+++ src/main/java/com/java/smart_garage/services/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -12,24 +13,25 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @PostMapping
     public void sendMailForCredentials(String email, String username, String password) {
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("karailiev1994@gmail.com");
-        message.setTo("plamenchipev16@gmail.com");
+        message.setTo(email);
         message.setSubject("New Registered User Credentials");
         message.setText("Username: " + username + "\n" + "Password: " + password);
 
         mailSender.send(message);
     }
 
+    @PostMapping
     public void sendMailForNewPassword(String email, String password) {
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("karailiev1994@gmail.com");
-        message.setTo("plamenchipev16@gmail.com");
+        message.setTo(email);
         message.setSubject("New Password");
-
 
         // Link -> Forgotten password -> User writes mail -> Send new password
         message.setText("Password: " + password);
