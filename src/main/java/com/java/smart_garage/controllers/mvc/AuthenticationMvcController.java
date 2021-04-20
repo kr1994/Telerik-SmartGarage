86a811f67c1687ea2d5d674934017b955d021fe5
kr1994 <karailiev1994@gmail.com>
2021-04-21 01:59:56 +0300

    Fixed registration bug

--- src/main/java/com/java/smart_garage/controllers/mvc/AuthenticationMvcController.java
+++ src/main/java/com/java/smart_garage/controllers/mvc/AuthenticationMvcController.java
@@ -3,7 +3,9 @@ package com.java.smart_garage.controllers.mvc;
 import com.java.smart_garage.ModelMaper.ModelConversionHelper;
 import com.java.smart_garage.configuration.AuthenticationHelper;
 import com.java.smart_garage.configuration.Md5Hashing;
+import com.java.smart_garage.contracts.serviceContracts.CredentialService;
 import com.java.smart_garage.contracts.serviceContracts.EmailService;
+import com.java.smart_garage.contracts.serviceContracts.PersonalInfoService;
 import com.java.smart_garage.contracts.serviceContracts.UserService;
 import com.java.smart_garage.exceptions.AuthenticationHelperException;
 import com.java.smart_garage.models.Credential;
@@ -32,15 +34,22 @@ public class AuthenticationMvcController {
     private final UserService userService;
     private final ModelConversionHelper modelConversionHelper;
     private final EmailService emailService;
+    private final PersonalInfoService personalInfoService;
+    private final CredentialService credentialService;
 
     @Autowired
     public AuthenticationMvcController(AuthenticationHelper authenticationHelper,
                                        UserService userService,
-                                       ModelConversionHelper modelConversionHelper, EmailService emailService) {
+                                       ModelConversionHelper modelConversionHelper,
+                                       EmailService emailService,
+                                       PersonalInfoService personalInfoService,
+                                       CredentialService credentialService) {
         this.authenticationHelper = authenticationHelper;
         this.userService = userService;
         this.modelConversionHelper = modelConversionHelper;
         this.emailService = emailService;
+        this.personalInfoService = personalInfoService;
+        this.credentialService = credentialService;
     }
 
     @ModelAttribute
@@ -84,8 +93,8 @@ public class AuthenticationMvcController {
     }
 
     @GetMapping("/register")
-    public String showRegisterPage(HttpSession session) {
-        session.setAttribute("registerDto", new PersonalInfoDto());
+    public String showRegisterPage(Model model) {
+        model.addAttribute("registerDto", new PersonalInfoDto());
         return "register";
     }
 
@@ -93,18 +102,18 @@ public class AuthenticationMvcController {
     public String handleRegisterCustomer(@Valid @ModelAttribute("registerDto") PersonalInfoDto dto,
                                          BindingResult bindingResult,
                                          HttpSession session) {
-
-        try {
-            User user = fillUser(dto, "Customer");
-            userService.create(user, authenticationHelper.tryGetUser(session));
-            emailService.sendMailForCredentials(user.getPersonalInfo().getEmail(),
-                                                user.getCredential().getUsername(),
-                                                user.getCredential().getPassword());
-        } catch (AuthenticationHelperException e) {
-            bindingResult.rejectValue("username", "auth_error", e.getMessage());
-            return "register";
+        if(bindingResult.hasErrors()) {
+            return "redirect:/";
         }
-        return "redirect:/login";
+
+        User user = fillUser(dto, "Customer");
+        User admin = fillUser(dto, "Employee");
+        userService.create(user, admin);
+        emailService.sendMailForCredentials(user.getPersonalInfo().getEmail(),
+                                            user.getCredential().getUsername(),
+                                            user.getCredential().getPassword());
+
+        return "redirect:/";
     }
 
     @PostMapping("/reset_password")
@@ -131,12 +140,14 @@ public class AuthenticationMvcController {
     private User fillUser(PersonalInfoDto dto, String role) {
         User user = new User();
         PersonalInfo p = modelConversionHelper.personalInfoFromDto(dto);
+
         user.setPersonalInfo(p);
         Credential credential = new Credential();
         credential.setUsername(extractUsernameFromEmail(user.getPersonalInfo().getEmail()));
         String password = Md5Hashing.generateNewPassword(8);
         credential.setPassword(Md5Hashing.md5(password));
         user.setCredential(credential);
+
         UserType usertype = new UserType();
         usertype.setType(role);
         user.setUserType(usertype);
